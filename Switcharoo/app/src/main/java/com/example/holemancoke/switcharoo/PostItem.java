189a55be551c3458054a19b7d11e45ca7dfc7a9e
Henry Coleman <colemahe@oregonstate.edu>
2018-06-20 17:39:22 -0700

    ..

---
+++ Switcharoo/app/src/main/java/com/example/holemancoke/switcharoo/PostItem.java
@@ -0,0 +1,13 @@
+package com.example.holemancoke.switcharoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_item);
+    }
+}
